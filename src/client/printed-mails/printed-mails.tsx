69534b3b293d24a1e8e94afafbccbd602618bd43
Stanislaw Jagiella <Stanislaw.Jagiella@gmail.com>
2020-06-29 22:36:09 +0200

    + fix search icon

--- src/client/printed-mails/printed-mails.tsx
+++ src/client/printed-mails/printed-mails.tsx
@@ -13,96 +13,99 @@ import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import SearchIcon from "@material-ui/icons/Search";
 import {TTheme} from "../theme";
+import IconButton from '@material-ui/core/IconButton';
 
 const useStyles = makeStyles((theme: TTheme) => {
-  return {
-    MainVertical: {
-      display: 'flex',
-      flexDirection: 'column',
-      height: 'calc(100vh - 20px)'
-    },
-    Header: {
-      height: '70px',
-    },
-    Main: {
-      flex: 1,
-      position: 'relative',
-      backgroundColor: theme.palette.background.level2
-    },
-    ScrollContainer: {
-      position: 'absolute',
-      height: '100%',
-      overflowY: 'auto'
-    },
-    FooterContent: {
-      paddingTop: '10px',
-      paddingLeft: '10px',
-      fontSize: '10px',
-      fontWeight: 200
-    },
-    table: {
-      minWidth: 650,
+    return {
+        MainVertical: {
+            display: 'flex',
+            flexDirection: 'column',
+            height: 'calc(100vh - 20px)'
+        },
+        Header: {
+            height: '70px',
+        },
+        Main: {
+            flex: 1,
+            position: 'relative',
+            backgroundColor: theme.palette.background.level2
+        },
+        ScrollContainer: {
+            position: 'absolute',
+            height: '100%',
+            overflowY: 'auto'
+        },
+        FooterContent: {
+            paddingTop: '10px',
+            paddingLeft: '10px',
+            fontSize: '10px',
+            fontWeight: 200
+        },
+        table: {
+            minWidth: 650,
+        }
     }
-  }
 });
 
 export type TPrintedMailsProps = {
-  printedMailsApi: TPrintedMailsApi
+    printedMailsApi: TPrintedMailsApi
 }
 
 export const PrintedMails: FunctionComponent<TPrintedMailsProps> = ({printedMailsApi}) => {
-  const classes = useStyles();
-  const rows = printedMailsApi.response;
+    const cs = useStyles();
+    const rows = printedMailsApi.response;
 
-  const clickHandle: MouseEventHandler = (e) => {
-    printedMailsApi.api.downloadMails();
-  };
+    const clickHandle: MouseEventHandler = (e) => {
+        printedMailsApi.api.downloadMails();
+    };
 
-  return (
-      <div className={classes.MainVertical}>
-        <div className={classes.Header}>
-          <AppBar>
-            <Toolbar>
-              <SearchIcon onClick={clickHandle}/>
-            </Toolbar>
-          </AppBar>
+    return (
+        <div className={cs.MainVertical}>
+            <div className={cs.Header}>
+                <AppBar>
+                    <Toolbar>
+                        <IconButton color="inherit" aria-label="search" onClick={clickHandle}>
+                            <SearchIcon/>
+                        </IconButton>
+                    </Toolbar>
+                </AppBar>
+            </div>
+            <div className={cs.Main}>
+                <TableContainer className={cs.ScrollContainer} component={Paper}>
+                    <Table className={cs.table} size="small" aria-label="a dense table">
+                        <TableHead>
+                            <TableRow>
+                                <TableCell>timeStamp</TableCell>
+                                <TableCell align="right">pagesRanges</TableCell>
+                                <TableCell align="right">reason</TableCell>
+                                <TableCell align="right">sentDateMmtUtc</TableCell>
+                                <TableCell align="right">from</TableCell>
+                                <TableCell align="right">subject</TableCell>
+                                <TableCell align="right">filename</TableCell>
+                            </TableRow>
+                        </TableHead>
+                        <TableBody>
+                            {rows.map((row) => (
+                                <TableRow key={row.messageId}>
+                                    <TableCell component="th" scope="row">
+                                        {row.timeStamp}
+                                    </TableCell>
+                                    <TableCell align="right">{row.pagesRanges}</TableCell>
+                                    <TableCell align="right">{row.reason}</TableCell>
+                                    <TableCell align="right">{row.sentDateMmtUtc}</TableCell>
+                                    <TableCell align="right">{row.from}</TableCell>
+                                    <TableCell align="right">{row.subject}</TableCell>
+                                    <TableCell align="right">{row.fileName}</TableCell>
+                                </TableRow>
+                            ))}
+                        </TableBody>
+                    </Table>
+                </TableContainer>
+            </div>
+            <div className={cs.FooterContent}>
+                {"Version: 0.0.2"}
+            </div>
         </div>
-        <div className={classes.Main}>
-          <TableContainer className={classes.ScrollContainer} component={Paper}>
-            <Table className={classes.table} size="small" aria-label="a dense table">
-              <TableHead>
-                <TableRow>
-                  <TableCell>timeStamp</TableCell>
-                  <TableCell align="right">pagesRanges</TableCell>
-                  <TableCell align="right">reason</TableCell>
-                  <TableCell align="right">sentDateMmtUtc</TableCell>
-                  <TableCell align="right">from</TableCell>
-                  <TableCell align="right">subject</TableCell>
-                  <TableCell align="right">filename</TableCell>
-                </TableRow>
-              </TableHead>
-              <TableBody>
-                {rows.map((row) => (
-                    <TableRow key={row.messageId}>
-                      <TableCell component="th" scope="row">
-                        {row.timeStamp}
-                      </TableCell>
-                      <TableCell align="right">{row.pagesRanges}</TableCell>
-                      <TableCell align="right">{row.reason}</TableCell>
-                      <TableCell align="right">{row.sentDateMmtUtc}</TableCell>
-                      <TableCell align="right">{row.from}</TableCell>
-                      <TableCell align="right">{row.subject}</TableCell>
-                      <TableCell align="right">{row.fileName}</TableCell>
-                    </TableRow>
-                ))}
-              </TableBody>
-            </Table>
-          </TableContainer>
-        </div>
-        <div className={classes.FooterContent}>
-          {"Version: 0.0.2"}
-        </div>
-      </div>
-  );
+    );
 
 };
